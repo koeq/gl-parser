36fe2ff0821a20956f4ffc6a2de5fc9dac0a707d
koeq <koe.be@web.de>
2023-12-28 10:01:30 +0100

    add tokenization scaffolding

--- parse.go
+++ parse.go
@@ -4,9 +4,26 @@ import (
 	"errors"
 )
 
+// Weight Units
 const (
 	Kilogram = "kg"
 	Pound    = "lbs"
+)
+
+type TokenVariant string
+
+// Token Variants
+const (
+	Asperand     TokenVariant = "ASPERAND"
+	Asterisk     TokenVariant = "ASTERISK"
+	EOF          TokenVariant = "EOF"
+	ForwardSlash TokenVariant = "FORWARD_SLASH"
+	Hyphen       TokenVariant = "HYPHEN"
+	Newline      TokenVariant = "NEWLINE"
+	Number       TokenVariant = "NUMBER"
+	String       TokenVariant = "STRING"
+	WeightUnit   TokenVariant = "WEIGHT_UNIT"
+	Whitespace   TokenVariant = "WHITE_SPACE"
 )
 
 type Weight struct {
@@ -20,16 +37,35 @@ type Exercise struct {
 	reps   []int
 }
 
-type Token struct{}
+type Token struct {
+	variant TokenVariant
+	lexeme  string
+	line    int
+}
 
-type Scanner struct{}
+type Scanner struct {
+	tokens  []Token
+	start   int
+	current int
+	line    int
+}
+
+func (s *Scanner) isAtEnd(source string) bool {
+	return s.current >= len(source)
+}
+
+func (s *Scanner) tokenize(source string) (tokens []Token) {
+	for !s.isAtEnd(source) {
+		s.start = s.current
+		s.scan()
+	}
+
+	tokens = append(s.tokens, Token{variant: "EOF", lexeme: "", line: s.line})
+
+	return s.tokens
+}
 
 type Interpreter struct{}
-
-func (s *Scanner) Tokenize(source string) (tokens []Token) {
-
-	return []Token{}
-}
 
 func (i *Interpreter) interpret(tokens []Token) (exercises []Exercise, err error) {
 
@@ -41,7 +77,17 @@ func Parse(source string) (exercises []Exercise, err error) {
 		return nil, errors.New("empty source string")
 	}
 
-	tokens := (&Scanner{}).Tokenize(source)
+	tokens := (&Scanner{}).tokenize(source)
 
 	return (&Interpreter{}).interpret(tokens)
+}
+
+// Utils
+func isValidTokenVariant(tv TokenVariant) bool {
+	switch tv {
+	case Asperand, ForwardSlash, Asterisk, Number, String, Hyphen, WeightUnit, Whitespace:
+		return true
+	default:
+		return false
+	}
 }
